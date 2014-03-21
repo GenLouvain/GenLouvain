370455c76ff40f7f803a6b2e9bd02ee23893ca98
Lucas Jeub <jeub@maths.ox.ac.uk>
2014-03-21 16:46:51 +0000

    updated GenLouvain

--- MEX_SRC/compile_mex.m
+++ MEX_SRC/compile_mex.m
@@ -2,8 +2,8 @@
 ext=mexext;
 mkdir('../private')
 
-mex -largeArrayDims CXXFLAGS="\$CXXFLAGS -std=c++11" metanetwork_reduce.cpp full.cpp sparse.cpp group_index.cpp
+mex -largeArrayDims CXXFLAGS="\$CXXFLAGS -std=c++0x" metanetwork_reduce.cpp full.cpp sparse.cpp group_index.cpp
 movefile(['metanetwork_reduce.',ext],['../private/metanetwork_reduce.',ext]);
 
-mex -largeArrayDims CXXFLAGS="\$CXXFLAGS -std=c++11" group_handler.cpp full.cpp sparse.cpp group_index.cpp
+mex -largeArrayDims CXXFLAGS="\$CXXFLAGS -std=c++0x" group_handler.cpp full.cpp sparse.cpp group_index.cpp
 movefile(['group_handler.',ext],['../private/group_handler.',ext]);
