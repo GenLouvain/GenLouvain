69f814b47ac6f40fa993ece75d02375e35f6654f
Lucas Jeub <jeub@maths.ox.ac.uk>
2014-04-07 18:45:40 +0100

    Merge commit '3572fee4c686a2998618bcd76ab627c04163e0ba'

--- MEX_SRC/compile_mex.m
+++ MEX_SRC/compile_mex.m
@@ -2,8 +2,8 @@
 ext=mexext;
 mkdir('../private')
 
-mex -largeArrayDims CXXFLAGS="\$CXXFLAGS -std=c++0x" metanetwork_reduce.cpp full.cpp sparse.cpp group_index.cpp
+mex -largeArrayDims CXXFLAGS="\$CXXFLAGS -std=c++0x" -Imatlab_matrix metanetwork_reduce.cpp matlab_matrix/full.cpp matlab_matrix/sparse.cpp group_index.cpp
 movefile(['metanetwork_reduce.',ext],['../private/metanetwork_reduce.',ext]);
 
-mex -largeArrayDims CXXFLAGS="\$CXXFLAGS -std=c++0x" group_handler.cpp full.cpp sparse.cpp group_index.cpp
+mex -largeArrayDims CXXFLAGS="\$CXXFLAGS -std=c++0x" -Imatlab_matrix group_handler.cpp matlab_matrix/full.cpp matlab_matrix/sparse.cpp group_index.cpp
 movefile(['group_handler.',ext],['../private/group_handler.',ext]);
