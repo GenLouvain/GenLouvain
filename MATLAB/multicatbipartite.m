da9a8358d84e13cf85e95c5346fd0d0646a8c219
Lucas Jeub <jeub@maths.ox.ac.uk>
2014-09-11 16:51:40 +0100

    cleaned up documentation

--- MATLAB/multicatbipartite.m
+++ MATLAB/multicatbipartite.m
@@ -1,94 +1,41 @@
-function [B,twom] = multicatbipartite(A,gamma,omega)
-%MULTIORD  Multislice community detection for ordered slices, matrix version
-%   Version 0.99, August 26, 2011.
+function [B,twomu] = multicatbipartite(A,gamma,omega)
+% MULTICATBIPARTITE [B,twomu] = MULTICATBIPARTITE(A,gamma,omega)
 %
-%   [S,Q] = MULTICAT(A,OMEGA) with A a cell array of square symmetric
-%   matrices of equal size each representing an undirected network "slice"
-%   performs multislice community detection using the quality function
-%   described in Mucha et al. 2010, with interslice coupling OMEGA
-%   connecting all-to-all categorical slices.  Optimization is performed
-%   by the generalized Louvain code GENLOUVAINRAND by Jutla & Mucha
-%   (following from Blondel et al. 2008).  The output matrix S encodes the
-%   obtained community assignments, with S(i,s) identifying the community
-%   to which node i in slice s has been assigned.  The output Q gives the
-%   quality of the resulting partition of the network.  The sparse output
-%   matrix B is the equivalent quality/modularity matrix that can be used
-%   with other heuristics, ordering multislice nodes looping over identify
-%   followed by slices [corresponding to S(:) reordering].
+% Input: A: Cell array of MxN adjacncy matrices for each layer of a
+%           multilayer undirected bipartite network
+%        gamma: resolution parameter
+%        omega: interlayer coupling strength
 %
-%   See also
-%       multislice wrappers:        MULTICATF, MULTIORD, MULTIORDF
-%       other heuristics:           SPECTRAL23
-%       Kernighan-Lin improvement:  KLNB
+% Output: B: [(M+N)xT]x[(M+N)xT] flattened modularity tensor for the
+%            multilayer bipartite network with categorical coupling (T is
+%            the number of layers of the network)
+%         twomu: normalisation constant
 %
-%   Notes:
-%     The matrices in the cell array A are assumed to be symmetric, square,
-%     and of equal size.  These assumptions are not checked here.
+% Usage: [B,twomu]=multicatbipartite(A,gamma,omega);
+%        [S,Q]=genlouvain(B);
+%        Q=Q/twom;
+%        S=reshape(S,M+N,T);
 %
-%     This code assumes that the sparse quality/modularity matrix B will
-%     fit in memory and proceeds to build that matrix.  For larger systems,
-%     try MULTICATF.
+% References: 
+%       Barber, M. Modularity and community detection in bipartite networks. 
+%           Phys. Rev. E 76, 066102 (2007).
 %
-%     This code serves as a template and can be modified for situations
-%     with other wrinkles (e.g., different intraslice null models,
-%     different numbers of nodes from slice-to-slice, or systems which are
-%     both multiplex and longitudinal).  That is, this code is only a
-%     starting point; it is by no means exhaustive.
+%       Mucha, P. J., Richardson, T., Macon, K., Porter, M. A. & Onnela, J.-P. 
+%           Community structure in time-dependent, multiscale, and multiplex networks. 
+%           Science 328, 876?878 (2010).
 %
-%     This version can return different results from run to run because it
-%     considers nodes in pseudorandom (randperm) order.  Because of the
-%     potentially large number of nearly-optimal partitions (Good et al.
-%     2010), one is encouraged to investigate results of repeated
-%     applications of this and other codes.
-%
-%     The quality Q output here does not include the 1/(2*mu) prefactor.
-%
-%     Resolution parameters within each slice can be added in line 90.
-%
-%     By using this code, the user implicitly acknowledges that the authors
-%     accept no liability associated with that use.  (What are you doing
-%     with it anyway that might cause there to be a potential liability?!?)
-%
-%   References:
-%     Blondel, Vincent D., Jean-Loup Guillaume, Renaud Lambiotte, and
-%     Etienne Lefebvre, "Fast unfolding of communities in large networks,"
-%     Journal of Statistical Mechanics: Theory and Experiment, P10008
-%     (2008).
-%
-%     Fortunato, Santo, "Community detection in graphs," Physics Reports
-%     486, 75-174 (2010).
-%
-%     Good, Benjamin H., Yves-Alexandre de Montjoye, and Aaron Clauset,
-%     "Performance of modularity maximization in practical contexts,"
-%     Physical Review E 81, 046106 (2010).
-%
-%     Mucha, Peter J., Thomas Richardson, Kevin Macon, Mason A. Porter, and
-%     Jukka-Pekka Onnela. "Community Structure in Time-Dependent,
-%     Multiscale, and Multiplex Networks," Science 328, 876-878 (2010).
-%
-%     Porter, M. A., J. P. Onnela, and P. J. Mucha, "Communities in
-%     networks," Notices of the American Mathematical Society 56, 1082-1097
-%     & 1164-1166 (2009).
-%
-%   Acknowledgments:
-%     Thank you to Dani Bassett, Jesse Blocher, Bruce Rogers, and Simi Wang
-%     for their collaborative help which led to significant cleaning up
-%     of earlier versions of our multislice community detection codes.
-%
-%   Citation: If you use this code, please cite as
-%       Inderjit S. Jutla and Peter J. Mucha, "A generalized Louvain method
-%       for community detection implemented in MATLAB,"
-%       http://netwiki.amath.unc.edu/GenLouvain (2011).
+% Lucas Jeub
+% jeub@maths.ox.ac.uk
 
 [m,n]=size(A{1});
 N=m+n;
 T=length(A);
 B=spalloc(N*T,N*T,2*m*n+N*T*(2*T-2));
 
-twom=0;
+mu=0;
 for s=1:T
     k=sum(A{s},2);
-    d=sum(A{s});
+    d=sum(A{s},1);
     mm=sum(k);
     
     if mm==0
@@ -101,9 +48,9 @@ for s=1:T
     B(indx+1:indx+m,indx+m+1:indx+N)=B1;
     B(indx+m+1:indx+N,indx+1:indx+m)=B1';
     
-    twom=mm+twom;
+    mu=mm+mu;
 end
 all2all = N*[(-T+1):-1,1:(T-1)];
 B = B + omega*spdiags(ones(N*T,2*T-2),all2all,N*T,N*T);
-twom = 2*twom+2*(T-1)*T*N*omega;
+twomu = 2*mu+(T-1)*T*N*omega;
 end
--- MATLAB/multiordbipartite.m
+++ MATLAB/multiordbipartite.m
@@ -1,20 +1,40 @@
-function [B,twom] = multiordbipartite(A,gamma,omega)
-%MULTIORDBIPARTITE [B,TWOM]=MULTIORDBIPARTITE(A,gamma,omega)
+function [B,twomu] = multiordbipartite(A,gamma,omega)
+% MULTIORDBIPARTITE [B,twomu] = MULTIORDBIPARTITE(A,gamma,omega)
 %
-% Input: A: cell array of mxn bipartite adjacency matrices for each slice
-% of the network
+% Input: A: Cell array of MxN adjacncy matrices for each layer of a
+%           multilayer undirected bipartite network
 %        gamma: resolution parameter
-%        omega: interslice connection strength
+%        omega: interlayer coupling strength
+%
+% Output: B: [(M+N)xT]x[(M+N)xT] flattened modularity tensor for the
+%            multilayer bipartite network with ordinal coupling (T is
+%            the number of layers of the network)
+%         twomu: normalisation constant
+%
+% Usage: [B,twomu]=multiordbipartite(A,gamma,omega);
+%        [S,Q]=genlouvain(B);
+%        Q=Q/twom;
+%        S=reshape(S,M+N,T);
+%
+% References: 
+%       Barber, M. Modularity and community detection in bipartite networks. 
+%           Phys. Rev. E 76, 066102 (2007).
+%
+%       Mucha, P. J., Richardson, T., Macon, K., Porter, M. A. & Onnela, J.-P. 
+%           Community structure in time-dependent, multiscale, and multiplex networks. 
+%           Science 328, 876?878 (2010).
+%
+% Lucas Jeub
+% jeub@maths.ox.ac.uk
 
 [m,n]=size(A{1});
 N=m+n;
 T=length(A);
 B=spalloc(N*T,N*T,2*m*n*T+2*N*T);
-twom=0;
-
+mu=0;
 for s=1:T
     k=sum(A{s},2);
-    d=sum(A{s});
+    d=sum(A{s},1);
     mm=sum(k);
     
     if mm==0
@@ -27,11 +47,10 @@ for s=1:T
     B(indx+1:indx+m,indx+m+1:indx+N)=B1;
     B(indx+m+1:indx+N,indx+1:indx+m)=B1';
     
-    twom=mm+twom;
+    mu=mm+mu;
    
 end
 
 B = B + omega*spdiags(ones(N*T,2),[-N,N],N*T,N*T);
-twom=2*twom+2*N*T*omega;
-
+twomu=2*mu+2*N*(T-1)*omega;
 end
