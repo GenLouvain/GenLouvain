3f7aeed59af36be3d467b6db7be9bedaedee3ca6
Lucas Jeub <jeub@maths.ox.ac.uk>
2015-06-11 16:21:53 +0100

    convert modularity matrices for each layer (given by cellarray `B` into multilayer modularity matrix with categorical coupling of strength `omega`

---
+++ MATLAB/categorical_coupling.m
@@ -0,0 +1,12 @@
+function B=categorical_coupling(B,omega)
+% convert modularity matrices for each layer (given by cellarray `B` into
+% multilayer modularity matrix with categorical coupling of strength
+% `omega`
+
+T=length(B);
+N=length(B{1});
+all2all = N*[(-T+1):-1,1:(T-1)];
+
+B=blkdiag(B{:})+omega*spdiags(ones(N*T,2*T-2),all2all,N*T,N*T);
+
+end
