44b1780192c7841f31825c2379987a3568086efb
Lucas Jeub <jeub@maths.ox.ac.uk>
2015-06-20 23:14:05 +0100

    add optimisation flag

--- MEX_SRC/compile_mex.m
+++ MEX_SRC/compile_mex.m
@@ -9,9 +9,9 @@ if exist('OCTAVE_VERSION','builtin')
 
     mex -DOCTAVE -Imatlab_matrix group_handler.cpp matlab_matrix/full.cpp matlab_matrix/sparse.cpp group_index.cpp
 else
-    mex -largeArrayDims CXXFLAGS="\$CXXFLAGS -std=c++0x" -Imatlab_matrix metanetwork_reduce.cpp matlab_matrix/full.cpp matlab_matrix/sparse.cpp group_index.cpp
+    mex -largeArrayDims CXXFLAGS="\$CXXFLAGS -O4 -std=c++0x" -Imatlab_matrix metanetwork_reduce.cpp matlab_matrix/full.cpp matlab_matrix/sparse.cpp group_index.cpp
 
-    mex -largeArrayDims CXXFLAGS="\$CXXFLAGS -std=c++0x" -Imatlab_matrix group_handler.cpp matlab_matrix/full.cpp matlab_matrix/sparse.cpp group_index.cpp
+    mex -largeArrayDims CXXFLAGS="\$CXXFLAGS -O4 -std=c++0x" -Imatlab_matrix group_handler.cpp matlab_matrix/full.cpp matlab_matrix/sparse.cpp group_index.cpp
 end
 
 movefile(['metanetwork_reduce.',ext],['../private/metanetwork_reduce.',ext]);
