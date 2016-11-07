b807527308d888b4d1e382ef028bf9826de4065b
Lucas Jeub <ljeub@iu.edu>
2016-11-07 15:41:11 -0500

    inherit defaults from GenLouvain

--- iterated_genlouvain.m
+++ iterated_genlouvain.m
@@ -1,54 +1,40 @@
 function [S,Q,n_it]=iterated_genlouvain(B,limit,verbose,randord,randmove,S0,postprocessor,recursive)
 
 %set default for maximum size of modularity matrix
-if nargin<2||isempty(limit)
-    limit = 10000;
+if nargin<2
+    limit = [];
 end
 
 %set level of reported/displayed text output
-if nargin<3||isempty(verbose)
-    verbose = 1;
+if nargin<3
+    verbose = [];
 end
-if verbose
-    mydisp = @(s) disp(s);
-else
-    mydisp = @(s) disp('');
-end
+
 
 %set randperm- v. index-ordered
-if nargin<4||isempty(randord)
-    randord = 1;
-end
-if randord
-    myord = @(n) randperm(n);
-else
-    myord = @(n) 1:n;
+if nargin<4
+    randord = [];
 end
 
+
 %set move function (maximal (original Louvain) or random improvement)
-if nargin<5||isempty(randmove)
-    randmove=false;
-end
-if randmove
-    movefunction='moverand';
-else
-    movefunction='move';
+if nargin<5
+    randmove=[];
 end
 
 % set initial partition
-if nargin<6||isempty(S0)
+if nargin<6
     S0=[];
 end
 
 % set postprocessing function
-if nargin<7||isempty(postprocessor)
-    % default: do not do anything
-    postprocessor=@(S) S;
+if nargin<7
+    postprocessor=[];
 end
 
 % set recursive option
-if nargin<8||isempty(recursive)
-    recursive=false;
+if nargin<8
+    recursive=[];
 end
 
 S_old=[];
