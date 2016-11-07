6a7f6ed4a84c276a096cb063b31d25304740078b
Lucas Jeub <ljeub@iu.edu>
2016-11-07 15:40:20 -0500

    set C++ standard to C++11

--- MEX_SRC/compile_mex.m
+++ MEX_SRC/compile_mex.m
@@ -1,17 +1,17 @@
 % Compile mex
 ext=mexext;
 mkdir('../private');
-
+setenv('CXXFLAGS',[getenv('CXXFLAGS'),' -std=c++11']);
 if exist('OCTAVE_VERSION','builtin')
-    setenv('CXXFLAGS',[getenv('CXXFLAGS'),' -std=c++0x']);
+    
     
     mex -DOCTAVE -Imatlab_matrix metanetwork_reduce.cpp matlab_matrix/full.cpp matlab_matrix/sparse.cpp group_index.cpp
 
     mex -DOCTAVE -Imatlab_matrix group_handler.cpp matlab_matrix/full.cpp matlab_matrix/sparse.cpp group_index.cpp
 else
-    mex -largeArrayDims CXXFLAGS="\$CXXFLAGS -O4 -std=c++0x" -Imatlab_matrix metanetwork_reduce.cpp matlab_matrix/full.cpp matlab_matrix/sparse.cpp group_index.cpp
+    mex -largeArrayDims CXXFLAGS="\$CXXFLAGS" -Imatlab_matrix metanetwork_reduce.cpp matlab_matrix/full.cpp matlab_matrix/sparse.cpp group_index.cpp
 
-    mex -largeArrayDims CXXFLAGS="\$CXXFLAGS -O4 -std=c++0x" -Imatlab_matrix group_handler.cpp matlab_matrix/full.cpp matlab_matrix/sparse.cpp group_index.cpp
+    mex -largeArrayDims CXXFLAGS="\$CXXFLAGS -O4" -Imatlab_matrix group_handler.cpp matlab_matrix/full.cpp matlab_matrix/sparse.cpp group_index.cpp
 end
 
 movefile(['metanetwork_reduce.',ext],['../private/metanetwork_reduce.',ext]);
