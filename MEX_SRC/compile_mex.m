a3c93ae18f8df0cacbfa308aff3badb8d1687c92
Lucas Jeub <ljeub@iu.edu>
2016-11-24 13:17:25 -0500

    add assignmentoptimal to compile script

--- MEX_SRC/compile_mex.m
+++ MEX_SRC/compile_mex.m
@@ -1,18 +1,27 @@
 % Compile mex
+% different options for 32bit and 64bit Matlab
 ext=mexext;
+switch ext
+    case {'mexw32','mexglx','mexmac','mexmaci'} %32bit
+        arraydims='-compatibleArrayDims';
+    case {'mexw64','mexa64','mexmaci64'} %64bit
+        arraydims='-largeArrayDims';
+    otherwise %potentially new architectures in the future
+        warning('unknown mexext %s, assuming 64bit',ext)
+        arraydims='-largeArrayDims';
+end
 mkdir('../private');
 setenv('CXXFLAGS',[getenv('CXXFLAGS'),' -std=c++11']);
 if exist('OCTAVE_VERSION','builtin')
-    
-    
     mex -DOCTAVE -Imatlab_matrix metanetwork_reduce.cpp matlab_matrix/full.cpp matlab_matrix/sparse.cpp group_index.cpp
-
     mex -DOCTAVE -Imatlab_matrix group_handler.cpp matlab_matrix/full.cpp matlab_matrix/sparse.cpp group_index.cpp
+    mex -DOCTAVE ../Assignment/assignmentoptimal.c
 else
-    mex -largeArrayDims CXXFLAGS="\$CXXFLAGS -O4" -Imatlab_matrix metanetwork_reduce.cpp matlab_matrix/full.cpp matlab_matrix/sparse.cpp group_index.cpp
-
-    mex -largeArrayDims CXXFLAGS="\$CXXFLAGS -O4" -Imatlab_matrix group_handler.cpp matlab_matrix/full.cpp matlab_matrix/sparse.cpp group_index.cpp
+    mex(arraydims,'CXXFLAGS="\$CXXFLAGS -O4"','-Imatlab_matrix','metanetwork_reduce.cpp', 'matlab_matrix/full.cpp', 'matlab_matrix/sparse.cpp', 'group_index.cpp')
+    mex(arraydims,'CXXFLAGS="\$CXXFLAGS -O4"', '-Imatlab_matrix', 'group_handler.cpp', 'matlab_matrix/full.cpp', 'matlab_matrix/sparse.cpp', 'group_index.cpp')
+    mex(arraydims,'../Assignment/assignmentoptimal.c')
 end
 
 movefile(['metanetwork_reduce.',ext],['../private/metanetwork_reduce.',ext]);
 movefile(['group_handler.',ext],['../private/group_handler.',ext]);
+movefile(['assignmentoptimal.',ext],['../private/assignmentoptimal.',ext]);
